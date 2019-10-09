769dd2d84179ff8d5e43c834d43dcc5d1b45e9a9
Alissa <acielecki@arcadia.edu>
2019-10-09 11:19:53 -0400

    Initial commit

---
+++ src/MorseCode.java
@@ -0,0 +1,45 @@
+import java.util.HashMap;
+public class MorseCode {
+	public static HashMap<String, String> hm = new HashMap<String, String>();
+	public static void main(String[] args) {
+		String[] alphabet = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+				"k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+				"w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8",
+				"9", "0"};
+
+		String[] morseCodes = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+				"....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.",
+				"--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-",
+				"-.--", "--..", ".----", "..---", "...--", "....-", ".....",
+				"-....", "--...", "---..", "----.", "-----"};
+		
+		loadMap(alphabet, morseCodes);
+		System.out.println(hm);
+		decode("-.-.-", "");
+	}
+	
+	public static void loadMap(String[] alphabet, String[] morseCodes) {
+		for(int i = 0; i < 36; i++) {
+			hm.put(morseCodes[i], alphabet[i]);
+		}
+	}
+	
+	public static void decode(String input, String output) {
+		String decoded = "";
+		String smallerInput = "";
+		if(input.equals("")) {
+			System.out.println(output);
+			return;
+		}
+		for(int i = 1; i <= input.length(); i++) {
+			smallerInput=input.substring(0, i);
+			if(hm.containsKey(smallerInput)) {
+				decode(input.substring(i), output + hm.get(smallerInput));
+			}
+			else {
+				decode(input.substring(i), output + "");
+			}
+		}
+	}
+
+}
